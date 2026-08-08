1f500ca1af503d8c590f7743ccb71b2a5796c7df
Fernando <team@lemontreesites.com>
2026-08-08 17:56:03 -0500

    Update package namespace and application ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ fun computeOfficialSigningCertSha256(): String {
 val officialSigningCertSha256 = computeOfficialSigningCertSha256()
 
 android {
-    namespace = "com.lemontreesites.zenlemontv"
+    namespace = "com.zenlemon.app"
     compileSdk = 36
 
     defaultConfig {
@@ -60,7 +60,7 @@ android {
         versionCode = 17
         versionName = "1.0.16"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        buildConfigField("String", "OFFICIAL_APPLICATION_ID", "\"com.lemontreesites.zenlemontv\"")
+        buildConfigField("String", "OFFICIAL_APPLICATION_ID", "\"com.zenlemon.app\"")
         buildConfigField("String", "OFFICIAL_SIGNING_CERT_SHA256", "\"$officialSigningCertSha256\"")
         buildConfigField("String", "APP_UPDATE_CHANNEL", "\"stable\"")
         buildConfigField("long", "BUILD_TIMESTAMP_UTC", "0L")
